d3cd8b1334b0683993dc650240f4171e7b5e0c1d
MyLanNguyen999 <mylannguyen192@gmail.com>
2025-03-14 13:34:52 -0400

    update name on footer per feedback

--- components/footer.tsx
+++ components/footer.tsx
@@ -18,7 +18,7 @@ const Footer = () => {
                 height={199}
               />
             </div>
-            <p className="text-lg font-heading font-bold">Elmvale District Garden Enthusiasts</p>
+            <p className="text-lg font-heading font-bold">Elmvale and District Garden Enthusiast's</p>
           </div>
           
           <div className="grid grid-cols-2 gap-4">

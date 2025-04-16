dd698deb9e406fea833604ae6396e0738c4b5ffc
fazilabdullasalam <fazilabdullasalamca@gmail.com>
2025-04-15 23:27:43 -0400

    changes

--- app/member-hub/page.tsx
+++ app/member-hub/page.tsx
@@ -215,15 +215,9 @@ export default function MemberHub() {
               </div>
             </div>
             
-<<<<<<< HEAD:app/member-hub/page.tsx
             {/* Right column with resource links - Updated hover animation to match Garden Club page */}
             <div className="md:w-1/2 md:pl-8 flex flex-col h-full">
               <div className="w-full h-full flex flex-col justify-center">
-=======
-            {/* Right column with resource links */}
-            <div className="md:w-1/2 md:pl-8 flex flex-col justify-between h-[350px] md:h-full py-6">
-              <div className="w-full">
->>>>>>> 8a329c87498905b530a96a9673386812e9303fa0:app/memberhub/page.tsx
                 {resourceItems.map((item) => {
                   const resource = getResourceByCategory(item.id);
                   
